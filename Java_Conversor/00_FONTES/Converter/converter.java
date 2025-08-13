85e5b4844eca798c60ef396df05497a6a43ce30c
Vitória Kereski da Rosa <138222391+vitoriakr@users.noreply.github.com>
2025-08-13 08:28:04 -0300

    começo do Sistema de Votação

---
+++ Java_Conversor/00_FONTES/Converter/converter.java
@@ -0,0 +1,9 @@
+public class converter {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
